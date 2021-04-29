92ebbb307cb8b81c7814c90573e2e6a23da38cc8
LuukFTF <lucasvdvegt@gmail.com>
2021-04-29 18:21:04 +0200

    opdracht 1 & 2 done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,51 @@
-//
-// vis element
-//
-const fish = document.querySelector("fish")
-fish.style.transform = `translate(200px, 100px)`
-fish.style.filter = `hue-rotate(200deg)`
+const game = document.querySelector("game");
 
-//
-// bubble element
-//
-const bubble = document.querySelector("bubble")
-bubble.style.transform = `translate(20px, 300px)`
+function initGame(){
+
+}
+
+function createFish(){
+    let fish = document.createElement("fish");
+
+    let posY = randomPosY();
+    let posX = randomPosX();
+
+    let hue = randomHue();
+
+    fish.style.transform = `translate(${posX}px, ${posY}px)`;
+    fish.style.filter = `hue-rotate(${hue}deg)`;
+
+    game.appendChild(fish);
+}
+
+function createBubble(){
+    let bubble = document.createElement("bubble");
+
+    let posY = randomPosY();
+    let posX = randomPosX();
+
+    bubble.style.transform = `translate(${posX}px, ${posY}px)`;
+    
+    game.appendChild(bubble);
+}
+
+
+function randomPosX(){
+    return Math.floor(Math.random() * window.innerWidth);
+}
+
+function randomPosY(){
+    return Math.floor(Math.random() * window.innerHeight);
+}
+
+function randomHue(){
+    return Math.floor(Math.random() * 360);
+}
+
+
+createFish();
+
+createBubble();
+
+
+
